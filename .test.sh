5914126f640da7910df63c59d518096eddc115ce
Roeland Hancock <rhancock@gmail.com>
2018-08-21 00:26:58 -0400

    Abort trap

--- .test.sh
+++ .test.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
-rm data/*
+err() {
+	exit 99
+}
+trap 'err' 0
+
+set -e
 ./scripts/make_times.sh
-if [ '454af68bab5189371773440787878250' != `md5 -q data/Letters.1d` ]; then
+if [ '454af68bab5189371773440787878250' != `openssl md5 -binary data/Letters.1d | xxd -p` ]; then
   exit 1
 fi
 
-if [ 'b0639bd4ac2173d91a7c0abb8fa8373d' != `md5 -q data/Numbers.1d` ]; then
+if [ 'b0639bd4ac2173d91a7c0abb8fa8373d' != `openssl md5 -binary data/Numbers.1d | xxd -p` ]; then
   exit 2
 fi
 
-if [ '9c96c154c4e0a8a25184145d07151506' != `md5 -q data/Sentences.1d` ]; then
+if [ '9c96c154c4e0a8a25184145d07151506' != `openssl md5 -binary data/Sentences.1d | xxd -p` ]; then
   exit 3
 fi
 
-if [ '49586bb5b2ee329206c3682702b9b1ee' != `md5 -q data/Words.1d` ]; then
+if [ '49586bb5b2ee329206c3682702b9b1ee' != `openssl md5 -binary data/Words.1d | xxd -p` ]; then
   exit 4
 fi
+
+echo "OK"
+
+trap : 0
+
